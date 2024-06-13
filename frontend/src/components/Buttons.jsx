b51d0bf87f451eb700ea98a9fceaecadb7f4e560
ELIAS <elias.alasia.moreno@gmail.com>
2024-06-13 18:16:22 -0500

    colocando las imagenes

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Buttons = () => (
   <div className="container">
-    <button className="yellow-button">Task Progress</button>
-    <button className="light-green-button">Task Completed</button>
-    <button className="soft-red-button">Task Won't Do</button>
-    <button className="light-gray-button">Task To Do</button>
-    <button className="light-orange-button">Add new task</button>
+    <button className="yellow-button">Task Progress
+      <img src="public/Time.svg" alt="icon" />
+    </button>
+    <button className="green-button">Task Completed
+      <img src="public/Done1.svg" alt="icon" />
+    </button>
+    <button className="red-button">Task Won't Do
+      <img src="public/close.svg" alt="icon" />
+    </button>
+    <button className="gray-button">Task To Do</button>
+    <button className="orange-button">Add new task
+      <img src="public/Add.svg" alt="icon" />
+    </button>
   </div>
 );
 
